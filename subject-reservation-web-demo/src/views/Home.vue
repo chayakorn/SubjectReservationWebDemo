7912657cd86c9c3eb13c8f1db238696498040d2f
Rabmeat <jing182544@gmail.com>
2021-08-07 11:55:47 +0700

    update title jefialienworld_

--- subject-reservation-web-demo/src/views/Home.vue
+++ subject-reservation-web-demo/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <br />
     <p style="font-family: Roboto; font-size: 18pt">
-      SIT Subject Reservation Web by jefi.alienworld
+      SIT Subject Reservation Web by jefi.alienworld_
     </p>
     <br />
     <v-row justify="center" align="top" no-gutters style="height: 150px">
